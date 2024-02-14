8a1f0882b460929240bb266ebd6fb17cf18ece1f
Devis Dumler <devisdumler@gmail.com>
2024-02-14 23:29:29 +0700

    fix: homepage

--- src/App.js
+++ src/App.js
@@ -1,9 +1,7 @@
 import { useTranslation } from "react-i18next";
 import "./App.css";
-import { useState } from "react";
 
 function App() {
-  const [locale, setLocale] = useState("en");
   const { t, i18n } = useTranslation();
 
   const changeLanguage = (lng) => {
@@ -12,16 +10,37 @@ function App() {
 
   return (
     <div className="App">
-      <h1>{t("greet")}</h1>
-      <button
+      <h1>{t("title")}</h1>
+      <div>
+        <button
         onClick={() => {
-          const local = locale === "en" ? "de" : "en";
-          changeLanguage(local);
-          setLocale(local);
+          changeLanguage("en");
         }}
       >
-        Change Locale
+        Change Locale to English
       </button>
+      <button
+        onClick={() => {
+          changeLanguage("pl");
+        }}
+      >
+        Change Locale to Polish
+      </button>
+      <button
+        onClick={() => {
+          changeLanguage("ru");
+        }}
+      >
+        Change Locale to Russian
+      </button>
+      <button
+        onClick={() => {
+          changeLanguage("uk");
+        }}
+      >
+        Change Locale to Ukrainian
+      </button>
+      </div>
     </div>
   );
 }
